37cc19ae8fea8cd61dc6dda69a53781d2a1f3948
Ubuntu <ubuntu@ip-172-31-11-162.us-east-2.compute.internal>
2021-02-01 13:09:55 +0000

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://andri.anproject.web.id';
-$config['base_url'] = 'http://localhost/kelurahan';
+$config['base_url'] = 'https://andri.anproject.web.id';
+// $config['base_url'] = 'http://localhost/kelurahan';
 
 /*
 |--------------------------------------------------------------------------
